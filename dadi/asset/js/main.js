f39c3c529750cd1481d5932e2083f90aecf2a304
StefanoNesi <sn.boolean@gmail.com>
2021-10-14 16:07:02 +0200

    Start dice exercise

---
+++ dadi/asset/js/main.js
@@ -0,0 +1,10 @@
+//Generate a random number from 1 to 6, both for player and computer. Determine the winner based on which score is higher.
+
+/*
+Tools:
+
+- Generate a random number -> Math.random();
+- Determine the winner -> If()
+
+*/
+
